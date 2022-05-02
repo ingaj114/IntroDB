f82b8a08135b3cd24e9d35f6b873e885f44c45c6
Ingaj114 <ingaj114@gmail.com>
2022-05-02 17:45:56 +0300

    DB

--- java/com/company/objects/Main.java
+++ java/com/company/objects/Main.java
@@ -10,23 +10,24 @@ import com.company.menu.StudentMenu;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-//        System.out.println("Choose an option: ");
-//        System.out.println("1. Login");
-//        System.out.println("2. Sign up");
-//        int option = scanner.nextInt();
-//
-//        switch (option) {
-//            case 1: if (Auth.login()) {
-//                StudentMenu.menu();
-//            } else {
-//                System.out.println("Invalid login credentials. Try again!");
-//            }
-//                break;
-//            case 2:
-//                Auth.signUp();
-//                break;
-//        }
+        System.out.println("Choose an option: ");
+        System.out.println("1. Login");
+        System.out.println("2. Sign up");
+        int option = scanner.nextInt();
+
+        switch (option) {
+            case 1: if (Auth.login()) {
+                System.out.println("Login successful.");
+            } else {
+                System.out.println("Invalid login credentials. Try again!");
+            }
+                break;
+            case 2:
+                Auth.signUp();
+                break;
+        }
         //StudentMenu.menu();
+        System.out.println();
         AuthorsMenu.menu();
 
     }
